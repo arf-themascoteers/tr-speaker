79c73d107f47c491efbfd5477e0fb1e2f14e23a0
arif <OrangeBlu20R>
2021-05-27 06:44:03 +1000

    Base

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ test_x,test_y = savee.get_mel_data("test")
 train_x = preprocessor.preprocess_mel(train_x)
 test_x = preprocessor.preprocess_mel(test_x)
 
-nn = cnn.CNN(train_x, train_y, test_x, test_y)
+nn = None # cnn.CNN(train_x, train_y, test_x, test_y)
 nn.train()
 
 accuracy, loss = nn.test()
